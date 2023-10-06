dc4239e1902e092c642ea1dd84bb4ebe6c892990
Luan Victor <luanvitorlima2017@gmail.com>
2023-10-05 22:17:41 -0300

    aplicando restriçoes de requisitos

--- FilmesApi/Models/Filme.cs
+++ FilmesApi/Models/Filme.cs
@@ -1,8 +1,14 @@
-﻿namespace FilmesApi.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models;
 public class Filme
 {
+    [Required (ErrorMessage = "O titulo é obrigatório")]
     public string? Titulo { get; set; }
+    [Required(ErrorMessage = "O Genero é obrigatório")]
     public string? Genero { get; set; }
+    [Range(60, 2000, ErrorMessage = "o tamanho da duração não pode exceder 2000")]
+    [Required(ErrorMessage = "o tamanho da duração não pode exceder 2000")]
     public int? Duracao { get; set; }
 
 }
